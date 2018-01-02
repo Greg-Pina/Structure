c01aeadf2080098512d97bf3188509de2fc9a897
Greg Pina <pinagm2825@gmail.com>
2018-01-02 05:26:14 -0500

    Initial commit

---
+++ Structure/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmStructures
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNumber1 = New System.Windows.Forms.TextBox()
+        Me.txtNumber2 = New System.Windows.Forms.TextBox()
+        Me.txtNumber3 = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNumber1
+        '
+        Me.txtNumber1.Location = New System.Drawing.Point(165, 31)
+        Me.txtNumber1.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtNumber1.Name = "txtNumber1"
+        Me.txtNumber1.Size = New System.Drawing.Size(132, 22)
+        Me.txtNumber1.TabIndex = 1
+        '
+        'txtNumber2
+        '
+        Me.txtNumber2.Location = New System.Drawing.Point(165, 100)
+        Me.txtNumber2.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtNumber2.Name = "txtNumber2"
+        Me.txtNumber2.Size = New System.Drawing.Size(132, 22)
+        Me.txtNumber2.TabIndex = 2
+        '
+        'txtNumber3
+        '
+        Me.txtNumber3.Location = New System.Drawing.Point(165, 165)
+        Me.txtNumber3.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtNumber3.Name = "txtNumber3"
+        Me.txtNumber3.Size = New System.Drawing.Size(132, 22)
+        Me.txtNumber3.TabIndex = 3
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(39, 273)
+        Me.btnSubmit.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(87, 28)
+        Me.btnSubmit.TabIndex = 4
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(69, 39)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "First Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 108)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Second Number"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(63, 174)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Third Number"
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(49, 218)
+        Me.lblResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(249, 28)
+        Me.lblResult.TabIndex = 8
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(155, 277)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 24)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(282, 277)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmStructures
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(389, 327)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtNumber3)
+        Me.Controls.Add(Me.txtNumber2)
+        Me.Controls.Add(Me.txtNumber1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "frmStructures"
+        Me.Text = "Structures"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtNumber1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNumber2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNumber3 As System.Windows.Forms.TextBox
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Structure/Form1.vb
@@ -0,0 +1,116 @@
+﻿' *********************************************
+' Ray Harmon
+' <date>
+' This program shows how to create a Structure 
+' and pass it around instead of individual
+' variables.
+'
+' *********************************************
+
+Option Strict On
+
+Public Class frmStructures
+
+    Structure Numbers
+        Dim intNumber1 As Integer
+        Dim dblNumber2 As Double
+        Dim sngNumber3 As Single
+    End Structure
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        ' close the program
+        Close()
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'clear out all controls
+        txtNumber1.Clear()
+        txtNumber2.Clear()
+        txtNumber3.Clear()
+
+        lblResult.ResetText()
+
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        ' local variables
+        Dim Number As Numbers
+        Dim dblAnswer As Double
+
+
+
+        'If everything is validated then continue, else do not continue
+        If Validation(Number) = True Then
+
+            ' call function to add numbers together 
+            dblAnswer = Calculate(Number)
+
+            ' pass answer to be displayed. Pass it as a string
+            Display(dblAnswer.ToString)
+
+
+        End If
+
+    End Sub
+
+    Private Function Validation(ByRef Number As Numbers) As Boolean
+
+        ' this will validate the input data for text box 1 using IsNumeric
+        If IsNumeric(txtNumber1.Text) Then
+            Number.intNumber1 = CInt(txtNumber1.Text)
+        Else
+            MessageBox.Show("Please enter numbers only for Number 1.")
+            txtNumber1.Focus()
+            txtNumber1.BackColor = Color.Yellow
+            Return False ' reurn false as there is a problem
+        End If
+
+        ' this will validate the input data for text box 2 using IsNumeric
+        If IsNumeric(txtNumber2.Text) Then
+            Number.dblNumber2 = CDbl(txtNumber2.Text)
+        Else
+            MessageBox.Show("Please enter numbers only for Number 2.")
+            txtNumber2.Focus()
+            txtNumber2.BackColor = Color.Yellow
+            Return False    ' reurn false as there is a problem
+        End If
+
+        ' this will validate the input data for text box 3 using TryParse
+        If Not Single.TryParse(txtNumber3.Text, Number.sngNumber3) Then
+            MessageBox.Show("Please enter numbers only for Number 3.")
+            txtNumber3.Focus()
+            txtNumber3.BackColor = Color.Yellow
+            Return False    ' reurn false as there is a problem
+        End If
+
+        ' return True as we are good.
+        Return True
+
+    End Function
+
+    Private Function Calculate(ByVal Nums As Numbers) As Double
+
+        ' create local variable for answer
+        Dim dblAnswer As Double
+
+        'calculate
+        dblAnswer = Nums.sngNumber3 + Nums.dblNumber2 + Nums.intNumber1
+
+        ' return the answer as a double
+        Return dblAnswer
+
+
+    End Function
+
+    Private Sub Display(ByVal Answer As String)
+
+        ' display the output
+        lblResult.Text = Answer
+
+    End Sub
+
+End Class
